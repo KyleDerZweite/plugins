2863946f3af6c5fb702d7863b4da804930ca7bd3
Boy132 <mail@boy132.de>
2025-07-10 15:49:38 +0200

    fix and cleanup for UserCreatableServers

--- usercreatableservers/src/Filament/Components/Actions/CreateServerAction.php
+++ usercreatableservers/src/Filament/Components/Actions/CreateServerAction.php
@@ -2,13 +2,14 @@
 
 namespace Boy132\UserCreatableServers\Filament\Components\Actions;
 
+use App\Exceptions\Service\Deployment\NoViableAllocationException;
+use App\Exceptions\Service\Deployment\NoViableNodeException;
 use App\Filament\Server\Pages\Console;
 use App\Models\Egg;
 use App\Services\Servers\RandomWordService;
 use Boy132\UserCreatableServers\Models\UserResourceLimits;
 use Exception;
 use Filament\Actions\CreateAction;
-use Filament\Forms\Components\Group;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
@@ -30,72 +31,77 @@ class CreateServerAction extends CreateAction
 
         $this->model(UserResourceLimits::class);
 
-        $this->form([
-            Group::make([
+        $this->form(function () {
+            /** @var UserResourceLimits $userResourceLimits */
+            $userResourceLimits = UserResourceLimits::where('user_id', auth()->user()->id)->firstOrFail();
+
+            return [
                 TextInput::make('name')
                     ->label(trans('usercreatableservers::strings.name'))
                     ->required()
-                    ->default(fn () => (new RandomWordService())->word()),
+                    ->default(fn () => (new RandomWordService())->word())
+                    ->columnSpanFull(),
                 Select::make('egg_id')
                     ->label(trans('usercreatableservers::strings.egg'))
+                    ->prefixIcon('tabler-egg')
+                    ->options(fn () => Egg::all()->mapWithKeys(fn (Egg $egg) => [$egg->id => $egg->name]))
                     ->required()
-                    ->options(fn () => Egg::all()->mapWithKeys(fn (Egg $egg) => [$egg->id => $egg->name])),
+                    ->searchable()
+                    ->preload(),
                 TextInput::make('memory')
                     ->label(trans('usercreatableservers::strings.memory'))
                     ->required()
                     ->numeric()
-                    ->minValue(0)
-                    ->default(0)
-                    ->suffix(config('panel.use_binary_prefix') ? 'MiB' : 'MB')
-                    ->hint(trans('usercreatableservers::strings.hint_unlimited')),
+                    ->minValue(1)
+                    ->maxValue($userResourceLimits->getMemoryLeft())
+                    ->suffix(config('panel.use_binary_prefix') ? 'MiB' : 'MB'),
                 TextInput::make('disk')
                     ->label(trans('usercreatableservers::strings.disk'))
                     ->required()
                     ->numeric()
-                    ->minValue(0)
-                    ->default(0)
-                    ->suffix(config('panel.use_binary_prefix') ? 'MiB' : 'MB')
-                    ->hint(trans('usercreatableservers::strings.hint_unlimited')),
+                    ->minValue(1)
+                    ->maxValue($userResourceLimits->getDiskLeft())
+                    ->suffix(config('panel.use_binary_prefix') ? 'MiB' : 'MB'),
                 TextInput::make('cpu')
                     ->label(trans('usercreatableservers::strings.cpu'))
                     ->required()
                     ->numeric()
-                    ->minValue(0)
-                    ->default(0)
-                    ->suffix('%')
-                    ->hint(trans('usercreatableservers::strings.hint_unlimited')),
-            ])->columns(2),
-        ]);
+                    ->minValue(1)
+                    ->maxValue($userResourceLimits->getCpuLeft())
+                    ->suffix('%'),
+            ];
+        });
 
         $this->action(function (array $data) {
-            /** @var UserResourceLimits $userResourceLimits */
-            $userResourceLimits = UserResourceLimits::where('user_id', auth()->user()->id)->first();
-
             try {
-                $server = $userResourceLimits->createServer($data['name'], Egg::findOrFail($data['egg_id']), $data['memory'], $data['disk'], $data['cpu']);
+                /** @var UserResourceLimits $userResourceLimits */
+                $userResourceLimits = UserResourceLimits::where('user_id', auth()->user()->id)->firstOrFail();
+
+                if ($server = $userResourceLimits->createServer($data['name'], $data['egg_id'], $data['memory'], $data['disk'], $data['cpu'])) {
+                    redirect(Console::getUrl(panel: 'server', tenant: $server));
+                }
             } catch (Exception $exception) {
-                Notification::make()
-                    ->title('Could not create server')
-                    ->body($exception->getMessage())
-                    ->danger()
-                    ->send();
+                report($exception);
 
-                return;
-            }
-
-            if ($server) {
-                Notification::make()
-                    ->title('Server created')
-                    ->success()
-                    ->send();
-
-                redirect(Console::getUrl(panel: 'server', tenant: $server));
-            } else {
-                Notification::make()
-                    ->title('Could not create server')
-                    ->body('Not enough resources')
-                    ->danger()
-                    ->send();
+                if ($exception instanceof NoViableNodeException) {
+                    Notification::make()
+                        ->title('Could not create server')
+                        ->body('No viable node was found. Please contact the panel admin.')
+                        ->danger()
+                        ->send();
+                } elseif ($exception instanceof NoViableAllocationException) {
+                    Notification::make()
+                        ->title('Could not create server')
+                        ->body('No viable allocation was found. Please contact the panel admin.')
+                        ->danger()
+                        ->send();
+                } else {
+                    Notification::make()
+                        ->title('Could not create server')
+                        ->body('Unknown error. Please contact the panel admin.')
+                        ->danger()
+                        ->send();
+                }
             }
         });
     }
--- usercreatableservers/src/Providers/UserCreatableServersPluginProvider.php
+++ usercreatableservers/src/Providers/UserCreatableServersPluginProvider.php
@@ -4,19 +4,11 @@ namespace Boy132\UserCreatableServers\Providers;
 
 use App\Enums\HeaderActionPosition;
 use App\Enums\HeaderWidgetPosition;
-use App\Exceptions\Service\Deployment\NoViableAllocationException;
 use App\Filament\Admin\Resources\UserResource;
 use App\Filament\App\Resources\ServerResource\Pages\ListServers;
-use App\Filament\Server\Pages\Console;
-use App\Models\Egg;
 use Boy132\UserCreatableServers\Filament\Admin\Resources\UserResource\RelationManagers\UserResourceLimitRelationManager;
 use Boy132\UserCreatableServers\Filament\App\Widgets\UserResourceLimitsOverview;
-use Boy132\UserCreatableServers\Models\UserResourceLimits;
-use Exception;
-use Filament\Actions\Action;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
-use Filament\Notifications\Notification;
+use Boy132\UserCreatableServers\Filament\Components\Actions\CreateServerAction;
 use Illuminate\Support\ServiceProvider;
 
 class UserCreatableServersPluginProvider extends ServiceProvider
@@ -27,75 +19,7 @@ class UserCreatableServersPluginProvider extends ServiceProvider
 
         ListServers::registerCustomHeaderWidgets(HeaderWidgetPosition::Before, UserResourceLimitsOverview::class);
 
-        ListServers::registerCustomHeaderActions(HeaderActionPosition::Before,
-            Action::make('create_server')
-                ->visible(fn () => UserResourceLimits::where('user_id', auth()->user()->id)->exists())
-                ->form(function () {
-                    /** @var UserResourceLimits $userResourceLimits */
-                    $userResourceLimits = UserResourceLimits::where('user_id', auth()->user()->id)->firstOrFail();
-
-                    return [
-                        TextInput::make('name')
-                            ->label(trans('usercreatableservers::strings.name'))
-                            ->required()
-                            ->columnSpanFull(),
-                        Select::make('egg_id')
-                            ->label(trans('usercreatableservers::strings.egg'))
-                            ->prefixIcon('tabler-egg')
-                            ->options(fn () => Egg::all()->mapWithKeys(fn (Egg $egg) => [$egg->id => $egg->name]))
-                            ->required()
-                            ->searchable()
-                            ->preload(),
-                        TextInput::make('memory')
-                            ->label(trans('usercreatableservers::strings.memory'))
-                            ->required()
-                            ->numeric()
-                            ->minValue(1)
-                            ->maxValue($userResourceLimits->getMemoryLeft())
-                            ->suffix(config('panel.use_binary_prefix') ? 'MiB' : 'MB'),
-                        TextInput::make('disk')
-                            ->label(trans('usercreatableservers::strings.disk'))
-                            ->required()
-                            ->numeric()
-                            ->minValue(1)
-                            ->maxValue($userResourceLimits->getDiskLeft())
-                            ->suffix(config('panel.use_binary_prefix') ? 'MiB' : 'MB'),
-                        TextInput::make('cpu')
-                            ->label(trans('usercreatableservers::strings.cpu'))
-                            ->required()
-                            ->numeric()
-                            ->minValue(1)
-                            ->maxValue($userResourceLimits->getCpuLeft())
-                            ->suffix('%'),
-                    ];
-                })
-                ->action(function (array $data): void {
-                    try {
-                        /** @var UserResourceLimits $userResourceLimits */
-                        $userResourceLimits = UserResourceLimits::where('user_id', auth()->user()->id)->firstOrFail();
-
-                        if ($server = $userResourceLimits->createServer($data['name'], $data['egg_id'], $data['memory'], $data['disk'], $data['cpu'])) {
-                            redirect(Console::getUrl(panel: 'server', tenant: $server));
-                        }
-                    } catch (Exception $exception) {
-                        report($exception);
-
-                        if ($exception instanceof NoViableAllocationException) {
-                            Notification::make()
-                                ->title('Could not create server')
-                                ->body('No viable node was found. Please contact the panel admin.')
-                                ->danger()
-                                ->send();
-                        } else {
-                            Notification::make()
-                                ->title('Could not create server')
-                                ->body('Please contact the panel admin.')
-                                ->danger()
-                                ->send();
-                        }
-                    }
-                })
-        );
+        ListServers::registerCustomHeaderActions(HeaderActionPosition::Before, CreateServerAction::make());
     }
 
     public function boot(): void {}
