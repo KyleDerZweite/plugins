0777300681a55b2909b37f0356363599e83d0939
Boy132 <mail@boy132.de>
2025-12-25 00:51:07 +0100

    add subdomain relation manager to server (admin)

--- subdomains/lang/en/strings.php
+++ subdomains/lang/en/strings.php
@@ -6,7 +6,10 @@ return [
 
     'no_subdomains' => 'No Subdomains',
     'subdomain' => 'Subdomain|Subdomains',
-    'limit' => 'Subdomain Limit Reached',
+    'limit' => 'Limit',
+    'change_limit' => 'Change Limit',
+    'limit_changed' => 'Limit changed',
+    'limit_reached' => 'Subdomain Limit Reached',
     'create_subdomain' => 'Create Subdomain',
 
     'name' => 'Name',
